4bd2e59d5f121a4a2f2cb351a586f1d71ae3fa26
rlayfl <rlayfl@essex.ac.uk>
2024-10-19 13:48:32 +0100

    Added basic comment description of what the algorithm does

--- main.py
+++ main.py
@@ -22,10 +22,40 @@ def read_pixel_values_of_image(image):
     openedImage = Image.open(image)
     imagePixels = openedImage.load()
 
+    # For every row
     for x in range(512):
+        # For every column
         for y in range(512):
+
+            # If the pixel is pure red (might need changing to ensure other values with 255 R value aren't included)
             if imagePixels[x,y][0] == 255:
                 print("RED PIXEL FOUND")
+
+                # Check the values around it but one pixel away like this:
+
+                #       # # # # #
+                #       #       #
+                #       #   #   #
+                #       #       #
+                #       # # # # #
+
+
+                # Check the 16 pixels around it 1 layer away to see if they are also red
+                # Going clockwise, the pixels are:
+
+                # Row 0 would be checked like:
+
+                # - (x-2, y-2)
+                # - (x-1, y-2)
+                # - (x, y-2)
+                # - (x+1, y-2)
+                # - (x+2, y-2)
+
+                #Extend this to check the subsequent rows
+
+
+                
+
                 print("CHANGING TO GREEN")
                 imagePixels[x,y] = (0, 255, 0, 255)
 
